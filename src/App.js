d168fe0b481124242890e1042f0652a24f2e6d5c
nvizcarra <nicolasvizcarra64@gmail.com>
2021-05-07 20:05:51 -0300

    Add form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
+// 1° paso importar Emotion Styled
+import styled from '@emotion/styled';
 
+// 2° crear contenedores
+// Contenedor
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+// Formulario
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
+
+// 3° aplicar contenedores
 function App() {
   return (
-    <Header 
-      titulo='Cotizador de seguros'
-    />
+    // Contenedor
+    <Contenedor>
+        <Header 
+          titulo='Cotizador de seguros'
+      />
+    {/* Formulario */}
+      <ContenedorFormulario>
+          <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
