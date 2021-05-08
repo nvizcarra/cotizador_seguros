58cf247691def42553d9d888b3e8bb4d218aa2fd
nvizcarra <nicolasvizcarra64@gmail.com>
2021-05-08 14:36:57 -0300

    Styles for summary

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -1,4 +1,14 @@
 import React, { Fragment } from 'react';
+import styled from '@emotion/styled';
+import { primerMayuscula } from '../helper';
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838F;
+    color: #FFF;
+    margin-top: 1rem;
+`;
 
 const Resumen =  ({datos}) => {
     
@@ -9,14 +19,14 @@ const Resumen =  ({datos}) => {
 
 
     return (
-        <Fragment>
+        <ContenedorResumen>
             <h2>Resumen de Cotización</h2>
             <ul>
-                <li>Marca: </li>
-                <li>Modelo: </li>
-                <li>Plan: </li>
+                <li>Marca: {primerMayuscula(marca)}</li>
+                <li>Modelo: {modelo}</li>
+                <li>Plan: {primerMayuscula(plan)}</li>
             </ul>
-        </Fragment>
+        </ContenedorResumen>
     );
 }
 
--- src/helper.js
+++ src/helper.js
@@ -32,3 +32,8 @@ export function calcularMarca(marca) {
 export function obtenerPlan (plan) {
     return (plan === 'basico') ? 1.20 : 1.50;
 }
+
+// Muestra la primer letra mayúscula
+export function primerMayuscula (texto) {
+    return texto.charAt(0).toUpperCase() + texto.slice(1);
+}
